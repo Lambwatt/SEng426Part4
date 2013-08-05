ba4e3a04c1ee4ee882682232efec1ad5635e0926
Michael Pattie <mjpattie@w217-062.wireless.uvic.ca>
2013-08-05 14:25:22 -0700

    Completed ActionComboBox tests. Changed length of array created by createEditorColourActions

---
+++ SEngWorkspace2/JWP/com/jmonkey/office/jwp/support/MiniJWP.java
@@ -0,0 +1,13 @@
+package jmonkey.office.jwp.support;
+
+import java.awt.event.ActionListener;
+
+import jmonkey.office.jwp.JWP;
+
+public class MiniJWP extends JWP implements ActionListener {
+	
+	public MiniJWP()
+	{
+		super();
+	}
+}
